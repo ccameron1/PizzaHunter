0cef3696c55ecdee70be687f3f30e6bd9d58e93f
Carly Cameron <carlycameron01@gmail.com>
2019-05-22 14:18:40 -0500

    before he helps us

--- PizzaHunter/ViewController.swift
+++ PizzaHunter/ViewController.swift
@@ -94,11 +94,64 @@ class ViewController:  UIViewController, CLLocationManagerDelegate, MKMapViewDel
             return pin
         }
     }
+    
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        
+//        getAddressFromLatLon(pdblLatitude: "\(view.annotation?.coordinate.latitude)", withLongitude: "\(view.annotation?.coordinate.longitude)")
 //        view.annotation?.subtitle =
     }
 
-
+//
+//    func getAddressFromLatLon(pdblLatitude: String, withLongitude pdblLongitude: String) {
+//        var center : CLLocationCoordinate2D = CLLocationCoordinate2D()
+//        let lat: Double = Double("\(pdblLatitude)")!
+//        //21.228124
+//        let lon: Double = Double("\(pdblLongitude)")!
+//        //72.833770
+//        let ceo: CLGeocoder = CLGeocoder()
+//        center.latitude = lat
+//        center.longitude = lon
+//
+//        let loc: CLLocation = CLLocation(latitude:center.latitude, longitude: center.longitude)
+//
+//
+//        ceo.reverseGeocodeLocation(loc, completionHandler:
+//            {(placemarks, error) in
+//                if (error != nil)
+//                {
+//                    print("reverse geodcode fail: \(error!.localizedDescription)")
+//                }
+//                let pm = placemarks! as [CLPlacemark]
+//
+//                if pm.count > 0 {
+//                    let pm = placemarks![0]
+//                    print(pm.country)
+//                    print(pm.locality)
+//                    print(pm.subLocality)
+//                    print(pm.thoroughfare)
+//                    print(pm.postalCode)
+//                    print(pm.subThoroughfare)
+//                    var addressString : String = ""
+//                    if pm.subLocality != nil {
+//                        addressString = addressString + pm.subLocality! + ", "
+//                    }
+//                    if pm.thoroughfare != nil {
+//                        addressString = addressString + pm.thoroughfare! + ", "
+//                    }
+//                    if pm.locality != nil {
+//                        addressString = addressString + pm.locality! + ", "
+//                    }
+//                    if pm.country != nil {
+//                        addressString = addressString + pm.country! + ", "
+//                    }
+//                    if pm.postalCode != nil {
+//                        addressString = addressString + pm.postalCode! + " "
+//                    }
+//
+//
+//                    print(addressString)
+//                }
+//        })
+//
+//    }
 }
 
