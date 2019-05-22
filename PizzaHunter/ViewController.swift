d961732867fc107d1587603f3cba190f9d309187
Carly Cameron <carlycameron01@gmail.com>
2019-05-22 15:55:39 -0500

    info pop up has address

--- PizzaHunter/ViewController.swift
+++ PizzaHunter/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController:  UIViewController, CLLocationManagerDelegate, MKMapViewDel
     var locationManager = CLLocationManager()
     var currentLocation : CLLocation!
     var parks : [MKMapItem] = []
+    var address : String = ""
     
     
     
@@ -95,63 +96,36 @@ class ViewController:  UIViewController, CLLocationManagerDelegate, MKMapViewDel
         }
     }
     
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-//        getAddressFromLatLon(pdblLatitude: "\(view.annotation?.coordinate.latitude)", withLongitude: "\(view.annotation?.coordinate.longitude)")
-//        view.annotation?.subtitle =
+    func reverseGeocode(location: CLLocation) -> String{
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location) { (placemarks : [CLPlacemark]?, error : Error?) in
+            let placemark = placemarks?.first
+            if let subthoroughfare = placemark?.subThoroughfare {
+                self.address = "\(subthoroughfare) \(placemark!.thoroughfare!) \n \(placemark!.locality!), \(placemark!.administrativeArea!)"
+                print("\(self.address)")
+                let alertController = UIAlertController(title: "Address", message: "\(self.address)", preferredStyle: .alert)
+                let action1 = UIAlertAction(title: "Okay", style: .default) { (UIAlertAction) in
+                    
+                }
+                
+                alertController.addAction(action1)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                print("no subthroughoufare")
+            }
+        }
+        return self.address
     }
-
-//
-//    func getAddressFromLatLon(pdblLatitude: String, withLongitude pdblLongitude: String) {
-//        var center : CLLocationCoordinate2D = CLLocationCoordinate2D()
-//        let lat: Double = Double("\(pdblLatitude)")!
-//        //21.228124
-//        let lon: Double = Double("\(pdblLongitude)")!
-//        //72.833770
-//        let ceo: CLGeocoder = CLGeocoder()
-//        center.latitude = lat
-//        center.longitude = lon
-//
-//        let loc: CLLocation = CLLocation(latitude:center.latitude, longitude: center.longitude)
-//
-//
-//        ceo.reverseGeocodeLocation(loc, completionHandler:
-//            {(placemarks, error) in
-//                if (error != nil)
-//                {
-//                    print("reverse geodcode fail: \(error!.localizedDescription)")
-//                }
-//                let pm = placemarks! as [CLPlacemark]
-//
-//                if pm.count > 0 {
-//                    let pm = placemarks![0]
-//                    print(pm.country)
-//                    print(pm.locality)
-//                    print(pm.subLocality)
-//                    print(pm.thoroughfare)
-//                    print(pm.postalCode)
-//                    print(pm.subThoroughfare)
-//                    var addressString : String = ""
-//                    if pm.subLocality != nil {
-//                        addressString = addressString + pm.subLocality! + ", "
-//                    }
-//                    if pm.thoroughfare != nil {
-//                        addressString = addressString + pm.thoroughfare! + ", "
-//                    }
-//                    if pm.locality != nil {
-//                        addressString = addressString + pm.locality! + ", "
-//                    }
-//                    if pm.country != nil {
-//                        addressString = addressString + pm.country! + ", "
-//                    }
-//                    if pm.postalCode != nil {
-//                        addressString = addressString + pm.postalCode! + " "
-//                    }
-//
-//
-//                    print(addressString)
-//                }
-//        })
-//
-//    }
+    
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        var placeLat = view.annotation?.coordinate.latitude
+        var placeLong = view.annotation?.coordinate.longitude
+        var place = CLLocation(latitude: placeLat!, longitude: placeLong!)
+        let address2 = reverseGeocode(location: place)
+        
+    }
 }
 
