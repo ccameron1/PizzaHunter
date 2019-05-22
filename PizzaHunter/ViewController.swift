79522fc467eeab1ea5cb10c7412f2a4f06ac95df
Carly Cameron <carlycameron01@gmail.com>
2019-05-22 13:22:33 -0500

    mvp and strech 1

--- PizzaHunter/ViewController.swift
+++ PizzaHunter/ViewController.swift
@@ -7,12 +7,92 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController:  UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var currentLocation : CLLocation!
+    var parks : [MKMapItem] = []
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        mapView.delegate = self; locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+    
+    @IBAction func zoomButtonPressed(_ sender: Any) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+   
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Pizza"
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: span)
+        request.region = region
+        let search = MKLocalSearch(request: request)
+        
+        
+        search.start { (response, error) in
+            guard let response = response else {return}
+            for mapItem in response.mapItems {
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.first
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var pin = MKAnnotationView(annotation: annotation, reuseIdentifier: "pins")
+        
+        if let title = annotation.title, let actualTitle = title {
+            if actualTitle == "Pizza Hut" {
+                pin.image = UIImage(named: "3d profile")
+
+            }
+            else {
+               
+                pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+                
+                
+            }
+        }
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        if annotation.isEqual(mapView.userLocation) {
+            return nil
+        } else {
+            return pin
+        }
     }
 
 
